0ed80b21bcdf70ffaf2802b28926862453dd23a3
Morten von Werder <Morten.vonWerder@gbv.de>
2025-10-10 11:42:36 +0200

    update import skript

--- import-terminologies.sh
+++ import-terminologies.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/bash
 set -euo pipefail
 
-TERMINOLOGIES=$1
-
 importer=http://localhost:5020/
 DATA=${DATA:-data}
 STAGE=${STAGE:-stage}
@@ -11,19 +9,42 @@ CONFIG="./dockerfiles/config/config-docker-compose.ttl"
 
 POST() { curl --silent --fail -X POST "$@"; }
 PUT() { curl --silent --fail -X PUT "$@"; }
+GET() { curl --silent --fail "$@"; }
+import() {
+    echo "## Register terminologies"
+    PUT -d @$@ ${importer}terminology/ | jq -r '.[]|[.uri,.prefLabel.en]|@tsv'
+    echo
 
-echo "## Register terminologies"
-PUT -d @$TERMINOLOGIES ${importer}terminology/ | jq -r '.[]|[.uri,.prefLabel.en]|@tsv'
-echo
+    jq -r '.[]|[.uri,.distributions[0].download]|@tsv' $@ | while read -r uri download; do
+        id=${uri##*/}
+        echo
+        echo "## Receive and load $uri"
+        POST ${importer}terminology/${id}/receive?from=${download}
+        POST ${importer}terminology/${id}/load   
+    done
+}
 
 # reset config
 cp $DEFAULT $CONFIG
 
-jq -r '.[]|[.uri,.distributions[0].download]|@tsv' $TERMINOLOGIES | while read -r uri download; do
+if [[ $1 =~ ^http://bartoc.org/en/node/.* ]]; then
+    uri=$1
     id=${uri##*/}
+    download=$2
+
+    echo "## Register terminology"
+    PUT ${importer}terminology/${id} | jq -r '[.uri,.prefLabel.en]|@tsv'
     echo
     echo "## Receive and load $uri"
     POST ${importer}terminology/${id}/receive?from=${download}
     POST ${importer}terminology/${id}/load
-    cat $STAGE/terminology/${id}/skosmos.ttl >> $CONFIG    
-done
+    
+    
+else
+    TERMINOLOGIES=$1
+
+    import $TERMINOLOGIES
+fi
+
+GET ${importer}terminology/skosmos.ttl >> $CONFIG
+
